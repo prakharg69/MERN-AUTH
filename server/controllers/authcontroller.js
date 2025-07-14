5237e04b324c7ce8fe37fa9383dc6caabea24d12
Prakhar Gussain <prakhargussain069@gmail.com>
2025-07-14 22:04:50 +0530

    feat(auth): implement logout functionality with token cookie clearance

--- server/controllers/authcontroller.js
+++ server/controllers/authcontroller.js
@@ -61,3 +61,15 @@ export const login = async (req, res) => {
   }
 };
 
+export const logout = async (req,res)=>{
+    try {
+        res.clearCookie('token',token, {
+      httpOnly: true,
+      secure: process.env.NODE_ENV === "production",
+      sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
+    })
+        return res.json({success:true,message:"Logged out"})
+    } catch (error) {
+        return res.json({ success: false, message: error.message });
+    }
+}
