f2e863f32fd59590624a1e0f2127f4c2ee8e3952
Prakhar Gussain <prakhargussain069@gmail.com>
2025-07-14 17:53:10 +0530

    Models Added

--- server/server.js
+++ server/server.js
@@ -2,9 +2,11 @@ import express from "express";
 import cors from "cors";
 import 'dotenv/config';
 import cookieParser from "cookie-parser";
+import connectDb from "./config/mongodb.js";
 
 const app = express();
 const port = process.env.PORT || 4000;
+connectDb();
 
 app.use(express.json());
 app.use(cors({ credentials: true }));
