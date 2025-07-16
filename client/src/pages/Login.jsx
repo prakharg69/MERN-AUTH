7b7656836f8c83e6ce0a31f9429bff0fc9118898
Prakhar Gussain <prakhargussain069@gmail.com>
2025-07-16 20:23:05 +0530

    feat: add onChange handlers for form inputs in login/signup component

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,39 +1,99 @@
-import React, { useState } from 'react'
-import { assets } from '../assets/assets';
+import React, { useState } from "react";
+import { assets } from "../assets/assets";
+import { Navigate } from "react-router-dom";
 
 function Login() {
-    const [state,setState] = useState('Sign Up');
+  const [state, setState] = useState("Sign Up");
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  console.log(name,email,password);
+  
+
   return (
-    <div className=' flex  items-center justify-center min-h-screen px-6  sm:px-0 bg-gradient-to-br from-blue-200 to-purple-400  gap-2'>
-        <img src={assets.logo} alt="" className="absolute left-5 sm:left-20 top-5 w-28 sm:w-32 cursor-pointer" />
-        <div className='bg-slate-900 p-10 rounded-lg shadow-lg w-full sm:w-96 text- text-indigo-300'>
-            <h2 className='text-3xl font-semibold text-white text-center mb-3'>{state==='Sign Up'?'Create Account':'Login'}</h2>
-            <p className='text-center mb-3 text-sm'>{state===' Sign Up'?'Create your account':'Login to your account'}</p>
-            <form action="">
-                <div className='mb-4 flex items-center gap-3 w-full px-5 py-2.5 rounded-full bg-[#333A5C]'>
-                    <img src={assets.person_icon} alt="" />
-                    <input className='bg-transparent outline-none' type="text " placeholder='Full Name' required/>
-                </div>
-                <div className='mb-4 flex items-center gap-3 w-full px-5 py-2.5 rounded-full bg-[#333A5C]'>
-                    <img src={assets.mail_icon} alt="" />
-                    <input className='bg-transparent outline-none' type="email " placeholder='Email Id' required/>
-                </div>
-                <div className='mb-4 flex items-center gap-3 w-full px-5 py-2.5 rounded-full bg-[#333A5C]'>
-                    <img src={assets.lock_icon} alt="" />
-                    <input className='bg-transparent outline-none' type="password" placeholder='Password' required/>
-                </div>
-                <p className='mb-4 text-indigo-500 cursor-pointer'>Forget password ?</p>
-                <button className='w-full py-2.5 rounded-full bg-gradient-to-r from-indigo-500 to-indigo-900 font-medium'>{state}</button>
-            </form>
-            <p className='text-gray-400 text-center  text-xs mt-4'>Alread have an account?{" "}
-                <span className='text-blue-400 cursor-pointer underline'>Login here</span>
-            </p>
-             <p className='text-gray-400 text-center  text-xs mt-4'>Dont't have an account?{" "}
-                <span className='text-blue-400 cursor-pointer underline'>Sign Up</span>
-            </p>
-        </div>
+    <div className="flex items-center justify-center min-h-screen px-6 sm:px-0 bg-gradient-to-br from-blue-200 to-purple-400 gap-2">
+      <img
+      onClick={()=> Navigate('/')}
+        src={assets.logo}
+        alt=""
+        className="absolute left-5 sm:left-20 top-5 w-28 sm:w-32 cursor-pointer"
+      />
+      <div className="bg-slate-900 p-10 rounded-lg shadow-lg w-full sm:w-96 text-indigo-300">
+        <h2 className="text-3xl font-semibold text-white text-center mb-3">
+          {state === "Sign Up" ? "Create Account" : "Login"}
+        </h2>
+        <p className="text-center mb-3 text-sm">
+          {state === "Sign Up"
+            ? "Create your account"
+            : "Login to your account"}
+        </p>
+        <form action="">
+          {state === "Sign Up" && (
+            <div className="mb-4 flex items-center gap-3 w-full px-5 py-2.5 rounded-full bg-[#333A5C]">
+              <img src={assets.person_icon} alt="" />
+              <input
+                className="bg-transparent outline-none"
+                type="text"
+                placeholder="Full Name"
+                required
+                value={name}
+                onChange={(e) => setName(e.target.value)}
+              />
+            </div>
+          )}
+          <div className="mb-4 flex items-center gap-3 w-full px-5 py-2.5 rounded-full bg-[#333A5C]">
+            <img src={assets.mail_icon} alt="" />
+            <input
+              className="bg-transparent outline-none"
+              type="email"
+              placeholder="Email Id"
+              required
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+          </div>
+          <div className="mb-4 flex items-center gap-3 w-full px-5 py-2.5 rounded-full bg-[#333A5C]">
+            <img src={assets.lock_icon} alt="" />
+            <input
+              className="bg-transparent outline-none"
+              type="password"
+              placeholder="Password"
+              required
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+          </div>
+          <p className="mb-4 text-indigo-500 cursor-pointer">
+            Forget password?
+          </p>
+          <button className="w-full py-2.5 rounded-full bg-gradient-to-r from-indigo-500 to-indigo-900 font-medium">
+            {state}
+          </button>
+        </form>
+        {state === "Sign Up" ? (
+          <p className="text-gray-400 text-center text-xs mt-4">
+            Already have an account?{" "}
+            <span
+              onClick={() => setState("Login")}
+              className="text-blue-400 cursor-pointer underline"
+            >
+              Login here
+            </span>
+          </p>
+        ) : (
+          <p className="text-gray-400 text-center text-xs mt-4">
+            Don’t have an account?{" "}
+            <span
+              onClick={() => setState("Sign Up")}
+              className="text-blue-400 cursor-pointer underline"
+            >
+              Sign Up
+            </span>
+          </p>
+        )}
+      </div>
     </div>
-  )
+  );
 }
 
-export default Login
+export default Login;
